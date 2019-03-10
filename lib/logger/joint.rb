eedea43c6103fe5b8109965c547b0bb60e5ba601
TOKI Yoshinori <toki@freedom.ne.jp>
2019-03-10 11:29:46 +0900

    initial template

---
+++ lib/logger/joint.rb
@@ -0,0 +1,8 @@
+require "logger/joint/version"
+
+module Logger
+  module Joint
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/logger/joint/version.rb
@@ -0,0 +1,5 @@
+module Logger
+  module Joint
+    VERSION = "0.1.0"
+  end
+end
